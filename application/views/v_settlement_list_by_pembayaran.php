7a91193cfa5591608f7fb23e0978ac3dfd470656
bagusprasojo <bagusprasojo@gmail.com>
2020-05-16 09:54:29 +0700

    penambahan list detail settlement per pembayaran

---
+++ application/views/v_settlement_list_by_pembayaran.php
@@ -0,0 +1,106 @@
+<?php $this->load->view("_partials/head.php") ?>
+
+<body>
+
+  <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <?php $this->load->view("_partials/sidebar.php") ?>
+    <!-- /#sidebar-wrapper -->
+
+    <!-- Page Content -->
+    <div id="page-content-wrapper">
+
+      <?php $this->load->view("_partials/navigation.php") ?>
+	  <div class="card-header">
+			<?php $this->load->view("_partials/breadcrumb.php") ?>
+		</div>
+			
+	  
+      <!-- DataTables -->
+				
+					<div class="card-header">
+						<a href="<?php echo site_url('pembayaran') ?>"><i class="fas fa-plus"></i> Menuju ke Daftar Pembayaran</a>
+					</div>
+					<div class="card-body">
+						<div class="table-responsive">
+							<table class="table table-sm table-bordered" id="dataTable" width="100%" cellspacing="0">
+								<thead>
+									<tr>
+										<th>No</th>
+                                        <th>No Settlement</th>
+										<th>Tanggal</th>
+                                        <th>No Pembayaran</th>
+										<th>No Piutang</th>
+										<th>Pembeli</th>
+										<th>Nominal</th>
+										<th>Keterangan</th>
+										<th>Action</th>
+									</tr>
+								</thead>
+								<tbody>
+									<?php 
+										$no = 1;
+										foreach ($settlements as $settlement): 
+									?>
+									<tr>
+										<td>
+											<?php echo $no++ ?>
+										</td>
+										
+										<td width="140">
+											<?php echo $settlement->no_transaksi ?>
+										</td>
+										<td width="100">
+											<?php echo $settlement->tgl_transaksi ?>
+										</td>
+										<td width="140">
+											<?php echo $settlement->no_pembayaran ?>
+										</td>
+										<td width="140">
+											<?php echo $settlement->no_piutang ?>
+										</td>
+										<td>
+											<?php echo $settlement->nama_pembeli ?>
+										</td>
+										<td  width="100" align="right">
+										<?php echo number_format($settlement->nominal) ?>
+										</td>
+										<td>
+											<?php echo $settlement->keterangan ?>
+										</td>
+										<td width="100">
+											<a onclick="deleteConfirm('<?php echo site_url('settlement/delete/'.$settlement->settlement_id) ?>')"
+												href="#!" class="btn btn-danger" > Hapus</a>
+										</td>
+									</tr>
+									<?php endforeach; ?>
+
+								</tbody>
+							</table>
+							
+						</div>
+				</div>
+      </div>
+    </div>
+    <!-- /#page-content-wrapper -->
+
+  </div>
+  <!-- /#wrapper -->
+
+  <?php $this->load->view("_partials/scrolltop.php") ?>
+  <?php $this->load->view("_partials/footer.php") ?>
+  <?php $this->load->view("_partials/modal.php") ?>
+  <?php $this->load->view("_partials/js.php") ?>
+
+  
+  <script>
+		function deleteConfirm(url){
+			$('#btn-delete').attr('href', url);
+			$('#deleteModal').modal();
+		}
+	</script>
+
+</body>
+
+</html>
